578b1b399f770f5c8dfc204edb0c8c877de4f890
dl129 <danny_ding129@foxmail.com>
2016-11-25 18:48:53 +0800

    module

--- src/Service/Cdn.php
+++ src/Service/Cdn.php
@@ -8,5 +8,37 @@ namespace Ksyun\Service;
 use Ksyun\Base\V4Curl;
 class Cdn extends V4Curl
 {
-	
+	protected function getConfig()
+	{
+		return [
+			'host' => '',
+            'timeout' => 5,  //此处设置无用，BaseCurl 没有设置
+            'config' => [
+                'headers' => [
+                    'Accept' => 'application/json'
+                ],
+            ],
+		];
+	}
+	protected $apiList =[
+		'GetCdnDomains' => [
+            'url' => '',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                   
+                ],
+            ],
+        ],
+		'AddDomain' => [
+			'url' => '',
+			'method' => '',
+			'config' => [
+				'query' => [
+				],
+			],
+		],
+	];
 }
+
+
