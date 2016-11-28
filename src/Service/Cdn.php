789ee71ca53426a209a4d51dc1db4cf62fbdb72f
dl129 <danny_ding129@foxmail.com>
2016-11-28 19:51:45 +0800

    complete 10 api

--- src/Service/Cdn.php
+++ src/Service/Cdn.php
@@ -22,6 +22,7 @@ class Cdn extends V4Curl
 		];
 	}
 	protected $apiList =[
+	    //域名列表
 		'GetCdnDomains' => [
             'url' => '/2016-09-01/domain/GetCdnDomains',
             'method' => 'get',
@@ -32,12 +33,103 @@ class Cdn extends V4Curl
                 ],
             ],
         ],
-		'AddDomain' => [
-			'url' => '',
-			'method' => '',
+		//新增域名
+		'AddCdnDomain' => [
+			'url' => '/2016-09-01/domain/AddCdnDomain',
+			'method' => 'get',
 			'config' => [
-				'query' => [
-				],
+				'header' => [
+                   'X-Version' => '2016-09-01',
+				   'X-Action'=>'AddCdnDomain',
+                ],
+			],
+		],
+		//查询域名基础信息
+		'GetCdnDomainBasic' => [
+			'url' => '/2016-09-01/domain/GetCdnDomainBasicInfo',
+			'method' => 'get',
+			'config' => [
+				'header' => [
+                   'X-Version' => '2016-09-01',
+				   'X-Action'=>'GetCdnDomainBasicInfo',
+                ],
+			],
+		],
+		//修改域名配置
+		'ModifyCdnDomain' => [
+			'url' => '/2016-09-01/domain/ModifyCdnDomainBasicInfo',
+			'method' => 'get',
+			'config' => [
+				'header' => [
+                   'X-Version' => '2016-09-01',
+				   'X-Action'=>'ModifyCdnDomainBasicInfo',
+                ],
+			],
+		],
+		//启用、停用某个加速域名 StartStopCdnDomain
+		'StartStopCdnDomain' => [
+			'url' => '/2016-09-01/domain/StartStopCdnDomain',
+			'method' => 'get',
+			'config' => [
+				'header' => [
+                   'X-Version' => '2016-09-01',
+				   'X-Action'=>'StartStopCdnDomain',
+                ],
+			],
+		],
+		//删除域名
+		'DeleteCdnDomain' => [
+			'url' => '/2016-09-01/domain/DeleteCdnDomain',
+			'method' => 'get',
+			'config' => [
+				'header' => [
+                   'X-Version' => '2016-09-01',
+				   'X-Action'=>'DeleteCdnDomain',
+                ],
+			],
+		],
+		//查询域名详细配置信息
+		'GetDomainConfigs' => [
+			'url' => '/2016-09-01/domain/GetDomainConfigs',
+			'method' => 'get',
+			'config' => [
+				'header' => [
+                   'X-Version' => '2016-09-01',
+				   'X-Action'=>'GetDomainConfigs',
+                ],
+			],
+		],
+		//设置过滤参数
+		'SetIgnoreQueryStringConfig' => [
+			'url' => '/2016-09-01/domain/SetIgnoreQueryStringConfig',
+			'method' => 'get',
+			'config' => [
+				'header' => [
+                   'X-Version' => '2016-09-01',
+				   'X-Action'=>'SetIgnoreQueryStringConfig',
+                ],
+			],
+		],
+		//设置回源 host
+		'SetBackOriginHostConfig' => [
+			'url' => '/2016-09-01/domain/SetBackOriginHostConfig',
+			'method' => 'get',
+			'config' => [
+				'header' => [
+                   'X-Version' => '2016-09-01',
+				   'X-Action'=>'SetBackOriginHostConfig',
+                ],
+			],
+		],
+		//设置refer防盗链 SetReferProtectionConfig
+		'SetReferProtectionConfig' => [
+			'url' => '/2016-09-01/domain/SetReferProtectionConfig',
+			'method' => 'get',
+			'config' => [
+				'header' => [
+                   'X-Version' => '2016-09-01',
+				   'X-Action'=>'SetReferProtectionConfig',
+                ],
 			],
 		],
 	];
--- tests/CdnTest.php
+++ tests/CdnTest.php
@@ -4,14 +4,135 @@ use Ksyun\Service\Cdn;
 
 class CdnTest extends \PHPUnit_Framework_TestCase
 {
+	//域名列表
 	public function testGetCdnDomains()
 	{
-		$params = {
+		$params = [
 			'query'=>[
-				
+				'PageSize'=>'20',
+				'PageNumber'=>'1',
+				'DomainName'=>'',
+				'DomainStatus'=>'online',
+				'CdnType'=>'download',
+				'FuzzyMatch'=>'',
 			],
-		};
+		];
 		$response = Cdn::getInstance()->request('GetCdnDomains', $params);
         return $this->assertEquals($response->getStatusCode(), 200);
 	}
+	//新增域名
+	public function testAddCdnDomain()
+	{
+		$params = [
+			'query'=>[
+				'DomainName'=>'www.qunar.com',
+				'CdnType'=>'download',
+				//'CdnSubType'=>'',
+				'CdnProtocol'=>'http',
+				'Regions'=>'CN',
+				'OriginType'=>'domain',
+				'OriginProtocol'=>'http',
+				'OriginPort'=>'80',
+				'Origin'=>'www.ksyun.com',
+			],
+		];
+		$response = Cdn::getInstance()->request('AddCdnDomain', $params);
+        return $this->assertEquals($response->getStatusCode(), 200);
+	}
+	//查询域名基础信息
+	public function testGetCdnDomainBasic()
+	{
+		$params = [
+			'query'=>[
+				'DomainId'=>'2D09QXK',
+			],
+		];
+		$response = Cdn::getInstance()->request('GetCdnDomainBasic', $params);
+        return $this->assertEquals($response->getStatusCode(), 200);
+	}
+	//修改域名配置
+	public function testModifyCdnDomain()
+	{
+		$params = [
+			'query'=>[
+				'DomainId'=>'2D09QXK',
+				'Origin'=>'www.ks-cdn.com',
+				'OriginType'=>'domain',
+				'OriginPort'=>'80',
+				//'Regions'=>'',
+			],
+		];
+		$response = Cdn::getInstance()->request('ModifyCdnDomain', $params);
+        return $this->assertEquals($response->getStatusCode(), 200);
+	}
+	//启用、停用某个加速域名 StartStopCdnDomain
+	public function testStartStopCdnDomain()
+	{
+		$params = [
+			'query'=>[
+				'DomainId'=>'2D09QXK',
+				'ActionType'=>'start',
+			],
+		];
+		$response = Cdn::getInstance()->request('StartStopCdnDomain', $params);
+        return $this->assertEquals($response->getStatusCode(), 200);
+	}
+	//删除域名,没有测试案例
+
+	//查询域名详细配置信息
+	public function testGetDomainConfigs()
+	{
+		$params = [
+			'query'=>[
+				'DomainId'=>'2D09QXK',
+				//'ConfigList'=>'cache_expired,cc,ignore_query_string',   //过滤规则
+			],
+		];
+		$response = Cdn::getInstance()->request('GetDomainConfigs', $params);
+        return $this->assertEquals($response->getStatusCode(), 200);
+	}
+	//设置过滤参数
+	public function testSetIgnoreQueryStringConfig()
+	{
+		$params = [
+			'query'=>[
+				'DomainId'=>'2D09QXK',
+				'Enable'=>'on',   //  on  或者 off
+			],
+		];
+		$response = Cdn::getInstance()->request('SetIgnoreQueryStringConfig', $params);
+        return $this->assertEquals($response->getStatusCode(), 200);
+	}
+	//设置回源 host
+	public function testSetBackOriginHostConfig()
+	{
+		$params = [
+			'query'=>[
+				'DomainId'=>'2D09QXK',
+				'BackOriginHost'=>'www.a.qunar.com',   //
+			],
+		];
+		$response = Cdn::getInstance()->request('SetBackOriginHostConfig', $params);
+        return $this->assertEquals($response->getStatusCode(), 200);
+	}
+	//
+	public function testSetReferProtectionConfig()
+	{
+		$params = [
+			'query'=>[
+				'DomainId'=>'2D09QXK',
+				'Enable'=>'on',   //
+				'ReferType'=>'block',
+				'ReferList'=>'www.baidu.com,www.sina.com',
+				//'AllowEmpty'=>'',  //默认 on
+			],
+		];
+		$response = Cdn::getInstance()->request('SetReferProtectionConfig', $params);
+        return $this->assertEquals($response->getStatusCode(), 200);
+	}
 }
+
+
+
+
+
