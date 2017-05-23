8f94d79adb4e501d328f125fe96677df30adf8bc
yangfan16 <yangfan9915@gmail.com>
2017-05-23 15:11:55 +0800

    kls php

--- src/Service/Kls.php
+++ src/Service/Kls.php
@@ -37,6 +37,7 @@ class Kls extends V4Curl
     }
 
     protected $apiList = [
+        //定时录制接口
         'CreateRecordTask' => [
             'url' => '/',
             'method' => 'post',
@@ -44,9 +45,10 @@ class Kls extends V4Curl
                 'query' => [
                     'Action' => 'CreateRecordTask',
                     'Version' => '2017-01-01'
-                ]
+                ],
             ],
         ],
+        //定时录制取消接口
         'CancelRecordTask' => [
             'url' => '/',
             'method' => 'post',
@@ -54,9 +56,10 @@ class Kls extends V4Curl
                 'query' => [
                     'Action' => 'CancelRecordTask',
                     'Version' => '2017-01-01'
-                ]
+                ],
             ],
         ],
+        //查询录像任务状态
         'GetRecordTask' => [
             'url' => '/',
             'method' => 'get',
@@ -64,9 +67,10 @@ class Kls extends V4Curl
                 'query' => [
                     'Action' => 'GetRecordTask',
                     'Version' => '2017-01-01'
-                ]
+                ],
             ],
         ],
+        //查询历史录制任务
         'ListHistoryRecordTasks' => [
             'url' => '/',
             'method' => 'get',
@@ -74,9 +78,10 @@ class Kls extends V4Curl
                 'query' => [
                     'Action' => 'ListHistoryRecordTasks',
                     'Version' => '2017-01-01'
-                ]
+                ],
             ],
         ],
+        //短视频开始录制
         'StartStreamRecord' => [
             'url' => '/',
             'method' => 'get',
@@ -84,9 +89,10 @@ class Kls extends V4Curl
                 'query' => [
                     'Action' => 'StartStreamRecord',
                     'Version' => '2017-01-01'
-                ]
+                ],
             ],
         ],
+        //短视频结束录制
         'StopStreamRecord' => [
             'url' => '/',
             'method' => 'post',
@@ -94,7 +100,62 @@ class Kls extends V4Curl
                 'query' => [
                     'Action' => 'StopStreamRecord',
                     'Version' => '2017-01-01'
-                ]
+                ],
+            ],
+        ],
+        //查询在线录制任务
+        'ListRecordingTasks' => [
+            'url' => '/',
+            'method' => 'get',
+            'cofig' => [
+                'query' => [
+                    'Action' => 'ListRecordingTasks',
+                    'Version' => '2017-01-01',
+                ],
+            ],
+        ],
+        //主播流时长统计
+        'ListStreamDurations' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ListStreamDurations',
+                    'Version' => '2017-01-01'
+                ],
+            ],
+        ],
+        //查询流实时信息
+        'ListRealtimePubStreamsInfo' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ListRealtimePubStreamsInfo',
+                    'Version' => '2017-01-01'
+                ],
+            ],
+        ],
+        //查询流历史信息
+        'ListHistoryPubStreamsInfo' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ListHistoryPubStreamsInfo',
+                    'Version' => '2017-01-01'
+                ],
+            ],
+        ],
+        //查询流历史错误信息
+        'ListHistoryPubStreamsErrInfo' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ListHistoryPubStreamsErrInfo',
+                    'Version' => '2017-01-01'
+                ],
             ],
         ],
     ];
--- tests/Kls.php
+++ tests/Kls.php
@@ -10,12 +10,92 @@ use Ksyun\Service\Kls;
 class KlsTest extends \PHPUnit_Framework_TestCase
 {
 	private $arrMethod = array(
-        'CreateRecordTask',			// 创建定时任务
-        'CancelRecordTask',			// 取消定时任务
-        'GetRecordTask',			// 获取任务详情
-        'ListHistoryRecordTasks',	// 历史任务列表
-        'StartStreamRecord',  		// 创建实时任务
-        'StopStreamRecord',			// 停止实时任务
+        'CreateRecordTask',                 // 创建定时任务
+        'CancelRecordTask',                 // 取消定时任务
+        'GetRecordTask',                    // 获取任务详情
+        'ListHistoryRecordTasks',           // 历史任务列表
+        'StartStreamRecord',                // 创建实时任务
+        'StopStreamRecord',                 // 停止实时任务
+        'ListRecordingTasks',               // 查询在线录制任务
+        'ListStreamDurations',              // 主播流时长统计
+        'ListRealtimePubStreamsInfo',       // 查询流实时信息
+        'ListHistoryPubStreamsInfo',        // 查询流历史信息
+        'ListHistoryPubStreamsErrInfo'      // 查询流历史错误信息
     );
+
+
+    public function testAllAPI()
+    {
+        foreach ($this->arrMethod as $method) {
+            $response = Offline::getInstance()->request($method);
+            return $this->assertEquals($response->getStatusCode(), 200);
+        }
+    }
+
+    public function testCreateRecordTask()
+    {
+        $response = kls::getInstance()->request('CreateRecordTask');
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testCancelRecordTask()
+    {
+        $response = kls::getInstance()->request('CancelRecordTask');
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testGetRecordTask()
+    {
+        $response = kls::getInstance()->request('GetRecordTask');
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testListHistoryRecordTasks()
+    {
+        $response = kls::getInstance()->request('ListHistoryRecordTasks');
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testStartStreamRecord()
+    {
+        $response = kls::getInstance()->request('StartStreamRecord');
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testStopStreamRecord()
+    {
+        $response = kls::getInstance()->request('StopStreamRecord');
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testListRecordingTasks()
+    {
+        $response = kls::getInstance()->request('ListRecordingTasks');
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testListStreamDurations()
+    {
+        $response = kls::getInstance()->request('ListStreamDurations');
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testListRealtimePubStreamsInfo()
+    {
+        $response = kls::getInstance()->request('ListRealtimePubStreamsInfo');
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testListHistoryPubStreamsInfo()
+    {
+        $response = kls::getInstance()->request('ListHistoryPubStreamsInfo');
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testListHistoryPubStreamsErrInfo()
+    {
+        $response = kls::getInstance()->request('ListHistoryPubStreamsErrInfo');
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
 	
 }
