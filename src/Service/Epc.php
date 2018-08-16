12bea5b18e3fc02bd19827c5eb74d838d3d2dcfe
jinwenpeng <jinwenpeng@kingsoft.com>
2018-08-16 17:54:34 +0800

    epc openapi配置完善

--- src/Service/Epc.php
+++ src/Service/Epc.php
@@ -31,82 +31,12 @@ class Epc extends V4Curl
     }
 
     protected $apiList = [
-        'StartEpc' => [
-            'url' => '/',
-            'method' => 'get',
-            'config' => [
-                'query' => [
-                    'Action' => 'StartEpc',
-                    'Version' => '2015-11-01'
-                ]
-            ],
-        ],
-        'ListEpcs' => [
-            'url' => '/',
-            'method' => 'get',
-            'config' => [
-                'query' => [
-                    'Action' => 'ListEpcs',
-                    'Version' => '2015-11-01'
-                ]
-            ],
-        ],
-        'UpdateEpc' => [
-            'url' => '/',
-            'method' => 'get',
-            'config' => [
-                'query' => [
-                    'Action' => 'UpdateEpc',
-                    'Version' => '2015-11-01'
-                ]
-            ],
-        ],
-        'GetEpc' => [
-            'url' => '/',
-            'method' => 'get',
-            'config' => [
-                'query' => [
-                    'Action' => 'GetEpc',
-                    'Version' => '2015-11-01'
-                ]
-            ],
-        ],
         'CreateEpc' => [
             'url' => '/',
             'method' => 'get',
             'config' => [
                 'query' => [
                     'Action' => 'CreateEpc',
-                    'Version' => '2015-11-01'
-                ]
-            ],
-        ],
-        'RebootEpc' => [
-            'url' => '/',
-            'method' => 'get',
-            'config' => [
-                'query' => [
-                    'Action' => 'RebootEpc',
-                    'Version' => '2015-11-01'
-                ]
-            ],
-        ],
-        'PoweroffEpc' => [
-            'url' => '/',
-            'method' => 'get',
-            'config' => [
-                'query' => [
-                    'Action' => 'PoweroffEpc',
-                    'Version' => '2015-11-01'
-                ]
-            ],
-        ],
-        'ReinstallEpc' => [
-            'url' => '/',
-            'method' => 'get',
-            'config' => [
-                'query' => [
-                    'Action' => 'ReinstallEpc',
                     'Version' => '2015-11-01'
                 ]
             ],
@@ -121,32 +51,312 @@ class Epc extends V4Curl
                 ]
             ],
         ],
-        'GetEpcImage' => [
+        'ModifyEpc' => [
             'url' => '/',
             'method' => 'get',
             'config' => [
                 'query' => [
-                    'Action' => 'GetEpcImage',
+                    'Action' => 'ModifyEpc',
                     'Version' => '2015-11-01'
                 ]
             ],
         ],
-        'MonitorEpc' => [
+        'ModifyHyperThreading' => [
             'url' => '/',
             'method' => 'get',
             'config' => [
                 'query' => [
-                    'Action' => 'MonitorEpc',
+                    'Action' => 'ModifyHyperThreading',
                     'Version' => '2015-11-01'
                 ]
             ],
         ],
-        'SwitchSubnet' => [
+        'ResetPassword' => [
             'url' => '/',
             'method' => 'get',
             'config' => [
                 'query' => [
-                    'Action' => 'SwitchSubnet',
+                    'Action' => 'ResetPassword',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DescribeEpcs' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DescribeEpcs',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'StartEpc' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'StartEpc',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'StopEpc' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'StopEpc',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'RebootEpc' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'RebootEpc',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'ReinstallEpc' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ReinstallEpc',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'ReinstallCustomerEpc' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ReinstallCustomerEpc',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'CreateImage' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'CreateImage',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'ModifyImage' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ModifyImage',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DeleteImage' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DeleteImage',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DescribeImages' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DescribeImages',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'ModifyNetworkInterfaceAttribute' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ModifyNetworkInterfaceAttribute',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'ModifyDns' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ModifyDns',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'ModifySecurityGroup' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ModifySecurityGroup',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'ImportKey' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ImportKey',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DeleteKey' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DeleteKey',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'ModifyKey' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ModifyKey',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'CreateKey' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'CreateKey',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DescribeKeys' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DescribeKeys',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DescribePhysicalMonitor' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DescribePhysicalMonitor',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DescribeCabinets' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DescribeCabinets',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'CreateRemoteManagement' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'CreateRemoteManagement',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'ModifyRemoteManagement' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'ModifyRemoteManagement',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DeleteRemoteManagement' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DeleteRemoteManagement',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'GetDynamicCode' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'GetDynamicCode',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DescribeRemoteManagements' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DescribeRemoteManagements',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DescribeEpcManagements' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DescribeEpcManagements',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DescribeVpns' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DescribeVpns',
+                    'Version' => '2015-11-01'
+                ]
+            ],
+        ],
+        'DescribeCertificates' => [
+            'url' => '/',
+            'method' => 'get',
+            'config' => [
+                'query' => [
+                    'Action' => 'DescribeCertificates',
                     'Version' => '2015-11-01'
                 ]
             ],
--- tests/EpcTest.php
+++ tests/EpcTest.php
@@ -4,9 +4,35 @@ use Ksyun\Service\Epc;
 
 class EpcTest extends \PHPUnit_Framework_TestCase
 {
-    public function testListEpcs()
+    public function testDescribeEpcs()
     {
-        $response = Epc::getInstance()->request('ListEpcs', [], 'cn-beijing-6');
+        $params = [
+            'v4_credentials' =>[
+                'ak' => 'your ak',
+                'sk' => 'your sk',
+            ],
+            'query' => [
+                'HostId.1' => '95220112-ab41-4595-85df-e6d04cb21784' //物理机ID
+            ],
+        ];
+        $response = Epc::getInstance()->request('DescribeEpcs', $params, 'cn-shanghai-3');
+        print_r(json_decode((string)$response->getBody(), true));
+        return $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testStopEpc()
+    {
+        $params = [
+            'v4_credentials' =>[
+                'ak' => 'your ak',
+                'sk' => 'your sk',
+            ],
+            'query' => [
+                'HostId' => '95220112-ab41-4595-85df-e6d04cb21784' //物理机ID
+            ],
+        ];
+        $response = Epc::getInstance()->request('StopEpc', $params, 'cn-shanghai-3');
+        print_r(json_decode((string)$response->getBody(), true));
         return $this->assertEquals($response->getStatusCode(), 200);
     }
 }
